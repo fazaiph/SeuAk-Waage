03324742a0ccd8ab79a1c7a2cc7b073ae57a1b4f
tetz96 <95231294+tetz96@users.noreply.github.com>
2022-11-16 12:14:12 +0100

    du hund

--- letztes/src/main.cpp
+++ letztes/src/main.cpp
@@ -3,6 +3,10 @@
 
 #define DOUT  3
 #define CLK  2
+//#define interrupt 4
+
+bool tareState;
+
 
 HX711 scale;
 
@@ -10,10 +14,29 @@ float calibration_factor = 447;
 // Attach the serial enabld LCD's RX line to digital pin 11
 SoftwareSerial LCD(10, 11); // Arduino SS_RX = pin 10 (unused), Arduino SS_TX = pin 11 
 
+/*void tareInterrupt()
+{
+  tareState = true;
+}
+
+ void whenInterrupptDO()
+ {
+    scale.tare();
+
+ } 
+*/
+
 void setup()
 {
   LCD.begin(9600); // set up serial port for 9600 baud
   delay(500); // wait for display to boot up
+  
+  /*pinMode(interrupt,INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(interrupt), tareInterrupt, RISING);
+
+
+*/
 
   scale.begin(DOUT, CLK);
   scale.set_scale();
@@ -37,12 +60,13 @@ void loop()
 
   scale.set_scale(calibration_factor); //Adjust to this calibration factor
   
-  LCD.print(scale.get_units(),2);
+  //LCD.print(scale.read_average(10));
+  LCD.print(scale.get_units(), 1);
   LCD.print(" g");
 
 
 
-  while (scale.get_units() > 200){
+  while (scale.get_units() > 2001){
     LCD.write("                  ");
     LCD.write("                  "); 
 
@@ -55,6 +79,12 @@ void loop()
     LCD.write("                  "); 
     
   }
-  delay(50);
+/*if (tareState){
+    whenInterrupptDO();
+  }
+*/
+
+  delay(500);
+  
   
 }
